66b38c522f4e629065198cd41ce9a3b7ce1f1210
peppapig450 <nbgenius1@gmail.com>
2025-01-22 14:36:39 -0800

    add tuples for lookup tables, and add the column filtering for the plant document

--- src/config.py
+++ src/config.py
@@ -1,24 +1,40 @@
-from utils.filtering import ColumnFilter, Operation
+from utils.column_filter import ColumnFilter, Operation
 
 # Column filters for our EIA860 files
+# Drop FERC columns for now
 EIA860_COLUMN_FILTERS = {
     "1___Utility_Y2023.xlsx": ColumnFilter(
         Operation.KEEP,
         [
             "Utility ID",
-            "Utility Name",
             "Street Address",
             "City",
             "State",
             "Zip",
             "Entity Type",
         ],
+        [("Utility ID", "Utility Name")],
     ),
     "2___Plant_Y2023.xlsx": ColumnFilter(
         Operation.DROP,
         [
             "Utility Name",
-            "Plant Name"
+            "Plant Name",
+            "Balancing Authority Name",
+            "Sector Name",
+            "FERC Cogeneration Status",
+            "FERC Cogeneration Docket Number",
+            "FERC Small Power Producer Status",
+            "FERC Small Power Producer Docket Number",
+            "FERC Exempt Wholesale Generator Status",
+            "FERC Exempt Wholesale Generator Docket Number",
+            "Transmission or Distribution System Owner"
+        ],
+        [
+            ("Plant Code", "Plant Name"),
+            ("Balancing Authority Code", "Balancing Authority Name"),
+            ("Sector", "Sector Name"),
+            ("Transmission or Distribution System Owner ID", "Transmission or Distribution System Owner")
         ],
     ),
 }
