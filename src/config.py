4f684c33b742b161abb67c6c2af7697ee660c89a
peppapig450 <nbgenius1@gmail.com>
2025-01-22 17:35:41 -0800

    add column filtering for wind power plants; move my comments around;  format properly

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 from utils.column_filter import ColumnFilter, Operation
 
+# Other ideas for simplification/brevity: addresses (state, county, etc.)
+
 # Column filters for our EIA860 files
-# Drop FERC columns for now
 EIA860_COLUMN_FILTERS = {
     "1___Utility_Y2023.xlsx": ColumnFilter(
         Operation.KEEP,
@@ -15,6 +16,7 @@ EIA860_COLUMN_FILTERS = {
         ],
         [("Utility ID", "Utility Name")],
     ),
+    # Drop FERC columns for now
     "2___Plant_Y2023.xlsx": ColumnFilter(
         Operation.DROP,
         [
@@ -22,23 +24,25 @@ EIA860_COLUMN_FILTERS = {
             "Plant Name",
             "Balancing Authority Name",
             "Sector Name",
-            "Pipeline Notes"
-            "FERC Cogeneration Status",
+            "Pipeline NotesFERC Cogeneration Status",
             "FERC Cogeneration Docket Number",
             "FERC Small Power Producer Status",
             "FERC Small Power Producer Docket Number",
             "FERC Exempt Wholesale Generator Status",
             "FERC Exempt Wholesale Generator Docket Number",
-            "Transmission or Distribution System Owner"
+            "Transmission or Distribution System Owner",
         ],
         [
             ("Plant Code", "Plant Name"),
             ("Balancing Authority Code", "Balancing Authority Name"),
             ("Sector", "Sector Name"),
-            ("Transmission or Distribution System Owner ID", "Transmission or Distribution System Owner")
+            (
+                "Transmission or Distribution System Owner ID",
+                "Transmission or Distribution System Owner",
+            ),
         ],
     ),
-    # Not sure how to handle Generator ID as there are duplicates
+    # Not sure how to handle Generator ID as there are duplicates, maybe break down by plant?
     "3_1_Generator_Y2023.xlsx": ColumnFilter(
         Operation.DROP,
         [
@@ -46,11 +50,22 @@ EIA860_COLUMN_FILTERS = {
             "Plant Name",
             "Unit Code",
             "Ownership",
-            'RTO/ISO LMP Node Designation',
-            'RTO/ISO Location Designation for Reporting Wholesale Sales Data to FERC',
+            "Sector Name",
+            "RTO/ISO LMP Node Designation",
+            "RTO/ISO Location Designation for Reporting Wholesale Sales Data to FERC",
         ],
-        
-    )
+    ),
+    # Handle the wind class with dataclass parsing
+    "3_2_Wind_Y2023.xlsx": ColumnFilter(
+        Operation.DROP,
+        [
+            "Utility Name",
+            "Plant Name",
+            "State",
+            "County",
+            "Sector Name",
+        ],
+    ),
 }
 
 EIA860_ZIP_FILE = "data/raw/eia860_data.zip"
